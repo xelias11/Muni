cdb98d02a070a1d3e6cb6ac1d741277342b70457
Vlado <elias@master.cz>
2015-10-11 10:37:08 +0200

    Initial commit

---
+++ app/src/androidTest/java/cz/muni/fi/pv256/movio/uco410422/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv256.movio.uco410422;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410422/Muni.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.pv256.movio.uco410422;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+/**
+ * Created by Vladimir on 21.09.2015.
+ */
+public class Muni extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		if (BuildConfig.DEBUG) {
+			initStrictMode();
+		}
+	}
+
+	private void initStrictMode() {
+		StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+				.detectAll()
+				.penaltyLog();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			tpb.penaltyFlashScreen();
+		}
+		StrictMode.setThreadPolicy(tpb.build());
+
+		StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+				.detectLeakedSqlLiteObjects()
+				.penaltyLog();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			vmpb.detectLeakedClosableObjects();
+		}
+		StrictMode.setVmPolicy(vmpb.build());
+	}
+}
